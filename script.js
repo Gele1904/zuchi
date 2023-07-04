41af2ee4102fa7b966e032f9a11b011664781072
Gele1904 <angelnineski@gmail.com>
2023-07-04 13:49:00 +0200

    Revert "test"
    This reverts commit 64eafa0778bf26d47539cae381b5caaa84e44c4d.

--- script.js
+++ script.js
@@ -1,38 +1,38 @@
-// // Image fullscreen
-// function openFullscreenImage(imageUrl) {
-//     var fullscreenContainer = document.createElement('div');
-//     fullscreenContainer.style.position = 'fixed';
-//     fullscreenContainer.style.top = '0';
-//     fullscreenContainer.style.left = '0';
-//     fullscreenContainer.style.width = '100%';
-//     fullscreenContainer.style.height = '100%';
-//     fullscreenContainer.style.backgroundColor = 'rgba(0, 0, 0, 0.9)';
-//     fullscreenContainer.style.display = 'flex';
-//     fullscreenContainer.style.alignItems = 'center';
-//     fullscreenContainer.style.justifyContent = 'center';
-//     fullscreenContainer.style.zIndex = '9999';
+// Image fullscreen
+function openFullscreenImage(imageUrl) {
+    var fullscreenContainer = document.createElement('div');
+    fullscreenContainer.style.position = 'fixed';
+    fullscreenContainer.style.top = '0';
+    fullscreenContainer.style.left = '0';
+    fullscreenContainer.style.width = '100%';
+    fullscreenContainer.style.height = '100%';
+    fullscreenContainer.style.backgroundColor = 'rgba(0, 0, 0, 0.9)';
+    fullscreenContainer.style.display = 'flex';
+    fullscreenContainer.style.alignItems = 'center';
+    fullscreenContainer.style.justifyContent = 'center';
+    fullscreenContainer.style.zIndex = '9999';
 
-//     var fullscreenImage = document.createElement('img');
-//     fullscreenImage.src = imageUrl;
-//     fullscreenImage.style.maxWidth = '100%';
-//     fullscreenImage.style.maxHeight = '100%';
+    var fullscreenImage = document.createElement('img');
+    fullscreenImage.src = imageUrl;
+    fullscreenImage.style.maxWidth = '100%';
+    fullscreenImage.style.maxHeight = '100%';
 
-//     fullscreenContainer.appendChild(fullscreenImage);
-//     document.body.appendChild(fullscreenContainer);
+    fullscreenContainer.appendChild(fullscreenImage);
+    document.body.appendChild(fullscreenContainer);
 
-//     fullscreenContainer.addEventListener('click', function () {
-//         document.body.removeChild(fullscreenContainer);
-//     });
-// }
+    fullscreenContainer.addEventListener('click', function () {
+        document.body.removeChild(fullscreenContainer);
+    });
+}
 
-// var designImages = document.querySelectorAll('.content img');
-// designImages.forEach(function (image) {
-//     image.addEventListener('click', function () {
-//         var imageUrl = image.getAttribute('src');
-//         openFullscreenImage(imageUrl);
-//     });
-// });
-// // Image fullscreeen end
+var designImages = document.querySelectorAll('.content img');
+designImages.forEach(function (image) {
+    image.addEventListener('click', function () {
+        var imageUrl = image.getAttribute('src');
+        openFullscreenImage(imageUrl);
+    });
+});
+// Image fullscreeen end
 
 // Contact
 function contact() {
